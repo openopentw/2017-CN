080d2f386261219882617597d6de6c764d3256ee
YJC(openopentw) <openopentw@gmail.com>
2017-10-18 23:45:15 +0800

    submitted files

---
+++ submit/hw1/hw1_b04902053/gen_ip.py
@@ -0,0 +1,86 @@
+dot_poses = []
+dot_pos = [0, 0, 0]
+def gen_dot_poses(step, be_head, be_three, pos):
+    """
+    first: gen_dot_poses(0, be_head, be_three, 0)
+
+    step: 0, 1, 2, 3(finish)
+    pos: e.g.
+        140112140112
+        oo.oo.oo.ooo
+        012345678901
+        dot_pos = [2, 5, 8]
+    """
+    global dot_poses
+    global dot_pos
+
+    if len(be_three) == 0:
+        return
+
+    if step == 3:
+        if len(be_three) == 1:
+            dot_poses += [list(dot_pos)]
+        elif len(be_three) < 3:
+            if be_head[0]:
+                dot_poses += [list(dot_pos)]
+        elif len(be_three) == 3:
+            if be_three[0]:
+                dot_poses += [list(dot_pos)]
+        return
+
+    if len(be_three) > (4 - step) * 3 or len(be_three) < 4 - step:
+        return
+
+    dot_pos[step] = pos + 1
+    gen_dot_poses(step + 1, be_head[1:], be_three[1:], pos + 1)
+    if be_head[0]:
+        dot_pos[step] = pos + 2
+        gen_dot_poses(step + 1, be_head[2:], be_three[2:], pos + 2)
+        if be_three[0]:
+            dot_pos[step] = pos + 3
+            gen_dot_poses(step + 1, be_head[3:], be_three[3:], pos + 3)
+
+def gen_be_head(string):
+    be_head = [True] * (len(string))
+    for i,_ in enumerate(be_head):
+        if string[i] == '0':
+            be_head[i] = False
+    return be_head
+
+def gen_be_three(string):
+    be_three = [False] * (len(string))
+    for i in range(len(be_three) - 2):
+        if string[i] == '1':
+            be_three[i] = True
+        elif string[i] == '2':
+            if string[i + 1] < '5':
+                be_three[i] = True
+            elif string[i + 1] == '5' and string[i + 2] < '6':
+                be_three[i] = True
+    return be_three
+
+def gen_ips(ip_str):
+    global dot_poses
+    global dot_pos
+
+    dot_poses = []
+    dot_pos = [0, 0, 0]
+
+    be_head = gen_be_head(ip_str)
+    be_three = gen_be_three(ip_str)
+    gen_dot_poses(0, be_head, be_three, 0)
+
+    ips = []
+    for dot_pos in dot_poses:
+        ips += [ip_str[:dot_pos[0]] + '.' + ip_str[dot_pos[0]:dot_pos[1]] + '.' + ip_str[dot_pos[1]:dot_pos[2]] + '.' + ip_str[dot_pos[2]:]]
+    return ips
+
+if __name__ == "__main__":
+    ip_str = '0000'
+    print(gen_ips(ip_str))
+    ip_str = '00000'
+    print(gen_ips(ip_str))
+    ip_str = '001000'
+    print(gen_ips(ip_str))
+    ip_str = '12345'
+    print(gen_ips(ip_str))
---
+++ submit/hw1/hw1_b04902053/main.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+
+import gen_ip
+
+import socket
+
+from random import randint
+from time import sleep
+
+import sys
+write = sys.stdout.buffer.write
+
+# host & names
+host = 'irc.freenode.net'
+port = 6667 # non-SSL
+channel = '#b04902053'
+botnick = 'PRIVMSG'
+
+with open('./config') as f:
+    channel = f.readline().split("'")[1]
+print(channel)
+
+# connect
+ircsocket = socket.SocketType(socket.AF_INET, socket.SOCK_STREAM)
+ircsocket.connect((host, port))
+
+ircsocket.send(bytes('NICK ' + botnick + '\n', 'UTF-8'))
+ircsocket.send(bytes('USER ' + botnick + ' ' + botnick + ' ' + botnick + ' ' + botnick + '\n', 'UTF-8'))
+# ircsocket.send(bytes('PRIVMSG nickserv :INOOPE\r\n'), 'UTF-8')
+
+def joinchan(chan):
+    """Join channel(s)."""
+    ircsocket.send(bytes('JOIN ' + chan + '\n', 'UTF-8'))
+    ircmsg = ''
+    while ircmsg.find('End of /NAMES list.') == -1:
+        ircmsg = ircsocket.recv(2048).decode('UTF-8')
+        ircmsg = ircmsg.strip('\n\r')
+        # print(ircmsg)
+
+def sendmsg(msg, target=channel):
+    """Send message to specified channel."""
+    msg = bytes('PRIVMSG ' + target + ' :' + msg + '\n', 'UTF-8')
+    sleep(randint(5, 10) / 10) # to avoid throttling due to flooding
+    write(msg)
+    ircsocket.send(msg)
+
+def ping(msg):
+    """Respond to server Pings."""
+    msg = msg[0:1] + 'O' + msg[2:]
+    ircsocket.send(bytes(msg, 'utf-8'))
+    sendmsg('This message should be eaten by irc. QQ.')
+
+def main():
+    print('Joining channel ...')
+    joinchan(channel)
+    print('Start!')
+    sendmsg('Hello! I am robot.')
+
+    while True:
+        ircmsg = ircsocket.recv(2048).decode('UTF-8')
+        ircmsg = ircmsg.strip('\n\r')
+        print(ircmsg)
+
+        if ircmsg.startswith('PING'):
+            print('=== PING PONG ===')
+            ping(ircmsg)
+
+        elif 'throttled due to flooding' in ircmsg:
+            print('=== THROTTLE DUE TO FLOODING ===')
+            # TODO
+            sleep(1)
+
+        elif ' PRIVMSG ' in ircmsg:
+            name = ircmsg.split('!', 1)[0][1:]
+            message = ircmsg.split(' PRIVMSG ', 1)[1].split(':', 1)[1]
+
+            print('{}: "{}"'.format(name, message))
+
+            if len(name) < 17:
+                if message[0] == '@':
+                    # repeat
+                    if message.startswith('@repeat '):
+                        print('=== repeat ===')
+                        sendmsg(message[8:])
+
+                    # convert
+                    elif message.startswith('@convert '):
+                        print('=== convert ===')
+                        num_str = message[9:]
+                        if num_str.startswith('0x'):
+                            num = int(num_str, 16)
+                            sendmsg(str(num))
+                        else:
+                            num = int(num_str)
+                            sendmsg('0x{}'.format(format(num, '0x')))
+
+                    # ip
+                    elif message.startswith('@ip '):
+                        print('=== ip ===')
+                        ip_str = message[4:]
+                        ips = gen_ip.gen_ips(ip_str)
+                        sendmsg(str(len(ips)))
+                        for ip in ips:
+                            print(ip)
+                            sendmsg(ip)
+
+                    # help
+                    elif message == '@help':
+                        print('=== help ===')
+                        sendmsg('@repeat <Message>')
+                        sendmsg('@convert <Number>')
+                        sendmsg('@ip <String>')
+
+                else:
+                    # print('=== parse name & message error ===')
+                    # print('got name: {}'.format(name))
+                    # print('got message: {}'.format(message))
+
+if __name__ == "__main__":
+    main()
